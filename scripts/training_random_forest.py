ce05771f91b0f86bc11cc6df53f1ab10f8ad7442
Ashan-Wimalasiri <e19452@ce.pdn.ac.lk>
2024-06-24 23:29:15 +0530

    Automated script update

--- scripts/training_random_forest.py
+++ scripts/training_random_forest.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-# Load the training dataa
+# Load the training data
 X_train = pd.read_csv('/home/e19452/ml_project/e19-co544-Diabetes-Health-Indicator/data/X_train.csv')
 y_train = pd.read_csv('/home/e19452/ml_project/e19-co544-Diabetes-Health-Indicator/data/y_train.csv').squeeze()  # Ensure y_train is a Series, not a DataFrame
 
